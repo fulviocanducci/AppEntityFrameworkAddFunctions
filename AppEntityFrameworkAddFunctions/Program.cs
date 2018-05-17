55f37810aabe895c0faf75836859b94a6c9e19b2
Fúlvio <fulviocanducci@hotmail.com>
2018-05-16 23:04:02 -0300

    Add project files.

---
+++ AppEntityFrameworkAddFunctions/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AppEntityFrameworkAddFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
